9f4bf5fe3f7af678a7517c93488dd1b3271a7c7c
Dev-Nadson <nadson.pros@gmail.com>
2025-10-16 10:41:26 -0300

    Feat: Controller adicionado

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,8 @@
 import type { FastifyInstance } from "fastify";
+import { list_user_controller } from "../controllers/users/list-user.controller.js";
 
 async function user_routes(app: FastifyInstance) {
-    app.get("/list", (req, reply) => { return reply.send("User") })
+    app.get("/", list_user_controller)
 }
 
 export { user_routes }
