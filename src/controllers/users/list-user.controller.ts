849b4f928b927426f190c47a0002587972b0823e
Dev-Nadson <nadson.pros@gmail.com>
2025-10-16 10:41:39 -0300

    Commit Inicial

---
+++ src/controllers/users/list-user.controller.ts
@@ -0,0 +1,7 @@
+import type { FastifyRequest, FastifyReply } from "fastify";
+
+async function list_user_controller(req: FastifyRequest, resply: FastifyReply) {
+
+}
+
+export { list_user_controller }
