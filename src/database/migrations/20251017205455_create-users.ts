24fd672f16a4281e8b510cb8ddb061c186e16a02
Dev-Nadson <nadson.pros@gmail.com>
2025-10-17 19:30:32 -0300

    Feat: Tabela de usuário inicializada

--- src/database/migrations/20251017205455_create-users.ts
+++ src/database/migrations/20251017205455_create-users.ts
@@ -1,15 +1,16 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.createTable("users", (table) => {
-        table.string("id").primary()
+        table.string("id").primary().notNullable()
         table.string("name").notNullable()
-        table.string("")
+        table.string("email").unique().notNullable()
+        table.string("password").notNullable()
+        table.timestamp("created_at").defaultTo(knex.fn.now()).notNullable()
+        table.timestamp("updated_at").defaultTo(knex.fn.now()).notNullable()
     })
 }
 
-
 export async function down(knex: Knex): Promise<void> {
+    knex.schema.dropTable("users")
 }
-
