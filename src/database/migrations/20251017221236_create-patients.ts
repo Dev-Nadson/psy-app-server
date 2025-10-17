bd7ef99ff12b7b8dc2af6ca9322a32c34eeb4a1c
Dev-Nadson <nadson.pros@gmail.com>
2025-10-17 19:31:09 -0300

    Feat: Tabela de pacientes inicializada

---
+++ src/database/migrations/20251017221236_create-patients.ts
@@ -0,0 +1,21 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+    knex.schema.createTable("patients", (table) => {
+        table.string("id").primary().notNullable()
+        table.string("psychologist_id").references("id").inTable("users").notNullable()
+        table.string("name").notNullable()
+        table.date('date_of_birth').notNullable()
+        table.string("gender").notNullable()
+        table.string("phone_number").unique().notNullable()
+        table.text("adress").notNullable()
+        table.text("observations").notNullable()
+        table.timestamp("created_at").defaultTo(knex.fn.now()).notNullable()
+        table.timestamp("updated_at").defaultTo(knex.fn.now()).notNullable()
+    })
+}
+
+export async function down(knex: Knex): Promise<void> {
+    knex.schema.dropTable("patients")
+}
+
