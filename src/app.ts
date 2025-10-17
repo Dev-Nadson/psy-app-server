163b995c88e2f79c8f0d0fb742e858b4ed417c98
Dev-Nadson <nadson.pros@gmail.com>
2025-10-17 11:19:54 -0300

    Feat: App numa função

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
-import fastify from "fastify";
+import fastify, { type FastifyInstance } from "fastify";
 import { app_routes } from "./routes/index.js";
+import { error_handler_middleware } from "./middlewares/error-handler.middleware.js";
 
-const app = fastify()
+async function create_app(): Promise<FastifyInstance> {
+    const app = fastify()
 
-app.register(app_routes)
+    app.register(app_routes)
+    app.setErrorHandler(error_handler_middleware)
+    return app
+}
 
-export { app }
+export { create_app }
