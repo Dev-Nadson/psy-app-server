56acc8be62349ea3d8c81c4907aa966ec0333ef4
Dev-Nadson <nadson.pros@gmail.com>
2025-10-18 21:28:00 -0300

    Feat: controllers adicionados

--- src/routes/professionals.routes.ts
+++ src/routes/professionals.routes.ts
@@ -1,11 +1,16 @@
 import type { FastifyInstance } from "fastify";
+import { list_professionals_controller } from "../controllers/professionals/list-professionals.controller.js";
+import { get_professionals_controller } from "../controllers/professionals/get-professionals.controller.js";
+import { create_professionals_controller } from "../controllers/professionals/create-professionals.controller.js";
+import { update_professionals_controller } from "../controllers/professionals/update-professionals.controller.js";
+import { delete_professionals_controller } from "../controllers/professionals/delete-professionals.controller.js";
 
 async function professionals_routes(app: FastifyInstance) {
-    app.get("/", () => { })
-    app.get("/:id", () => { })
-    app.post("/create", () => { })
-    app.get("/update", () => { })
-    app.get("/", () => { })
+    app.get("/", list_professionals_controller)
+    app.get("/:id", get_professionals_controller)
+    app.post("/create", create_professionals_controller)
+    app.get("/update", update_professionals_controller)
+    app.get("/delete", delete_professionals_controller)
 }
 
 export { professionals_routes }
