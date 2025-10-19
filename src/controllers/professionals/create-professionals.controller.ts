5d3587cd1ada7aa7fdca339e5eb9d91e11011b99
Dev-Nadson <nadson.pros@gmail.com>
2025-10-19 17:43:03 -0300

    Feat: Rota configurada e tratamento de erros configurado

--- src/controllers/professionals/create-professionals.controller.ts
+++ src/controllers/professionals/create-professionals.controller.ts
@@ -1,12 +1,15 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
 import { professional_schema } from "../../schemas/professionals.schema.js";
-import { creare_professional_repositorie } from "../../repositories/professionals/create-professionals.repo.js";
+import { create_professional_repositorie } from "../../repositories/professionals/create-professionals.repo.js";
 
 async function create_professionals_controller(req: FastifyRequest, reply: FastifyReply) {
     const { name, email, password } = professional_schema.parse(req.body)
-    const professional = await creare_professional_repositorie({ name, email, password })
+    const professional = await create_professional_repositorie({ name, email, password })
 
-    return reply.status(201).send(professional)
+    return reply.status(201).send({
+        "message": "Profissional criado com sucesso!",
+        "data": professional
+    })
 }
 
 export { create_professionals_controller }
--- src/repositories/professionals/create-professionals.repo.ts
+++ src/repositories/professionals/create-professionals.repo.ts
@@ -1,23 +1,40 @@
 import { Knex } from "../../database/config.js";
 import { create_id } from "../../utils/utils.js";
 import { hash_text } from "../../utils/encryptation.js";
+import { ConflictError, InternalServerError } from "../../utils/errors/app-errors.js";
+import type { IcreateProfessionalInput, ICreateProfessionalOutput } from "../../schemas/types/professionals.types.js";
 
-interface IcreateProfessionalInput {
-    name: string
-    email: string
-    password: string
-}
+async function create_professional_repositorie({ name, email, password }: IcreateProfessionalInput): Promise<ICreateProfessionalOutput> {
 
-async function creare_professional_repositorie({ name, email, password }: IcreateProfessionalInput) {
+    const exist_professional = await Knex("professionals").select("id").where({ email }).first()
+
+    if (exist_professional) {
+        throw new ConflictError("Email já cadastrado no sistema")
+    }
+
+    let hashed_password
+    try {
+        hashed_password = await hash_text(password)
+    } catch (error) {
+        console.error("Erro ao criptografar a senha:", error)
+        throw new InternalServerError("Falha ao processar a senha")
+    }
+
     const data = {
         id: create_id(),
         name,
         email,
-        password: await hash_text(password)
+        password: hashed_password
     }
 
     await Knex("professionals").insert(data)
-    return await Knex("professionals").select().where({ "id": data.id })
+    const created_professional = await Knex("professionals").select("id", "name", "email", "created_at").where({ id: data.id }).first()
+
+    if (!created_professional) {
+        throw new InternalServerError("Falha ao criar o profissional")
+    }
+
+    return created_professional
 }
 
-export { creare_professional_repositorie }
+export { create_professional_repositorie }
