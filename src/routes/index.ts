1818726d38b9786d24de402d042a59c8c160e1bf
Dev-Nadson <nadson.pros@gmail.com>
2025-10-18 21:06:20 -0300

    Feat: Rotas indexadas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,13 @@
 import type { FastifyInstance } from "fastify";
-import { user_routes } from "./user.routes.js";
+import { professionals_routes } from "./professionals.routes.js";
+import { anamnesis_routes } from "./anamnesis.routes.js";
+import { patients_routes } from "./patients.routes.js";
+
 
 async function app_routes(app: FastifyInstance) {
-    app.register(user_routes, { prefix: "users/" })
+    app.register(professionals_routes, { prefix: "professionals/" })
+    app.register(patients_routes, { prefix: "patients/" })
+    app.register(anamnesis_routes, { prefix: "anamnesis/" })
 }
 
 export { app_routes }
