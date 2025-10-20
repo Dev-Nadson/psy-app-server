bdf9ad066a451321149f4d39c4c91ff95ede8342
Dev-Nadson <nadson.pros@gmail.com>
2025-10-20 19:24:54 -0300

    Feat: Retorno adequado dos dados

--- src/repositories/professionals/get-professionals.repo.ts
+++ src/repositories/professionals/get-professionals.repo.ts
@@ -2,7 +2,7 @@ import { Knex } from "../../database/config.js";
 import { NotFoundError } from "../../utils/errors/app-errors.js";
 
 async function get_professionals_repository(id: string) {
-    const professional = await Knex("professionals").select().where({ id, deleted_at: null }).first()
+    const professional = await Knex("professionals").select("id", "name", "email", "created_at").where({ id, deleted_at: null }).first()
 
     if (!professional) {
         throw new NotFoundError("O usuário não foi encontrado")
--- src/repositories/professionals/list-professionals.repo.ts
+++ src/repositories/professionals/list-professionals.repo.ts
@@ -1,7 +1,8 @@
 import { Knex } from "../../database/config.js";
+import type { ICreateProfessionalOutput } from "../../schemas/types/professionals.types.js";
 
 async function list_professionals_repository() {
-    const professionals = await Knex("professionals").select()
+    const professionals = await Knex("professionals").select("id", "name", "email", "created_at").where({ deleted_at: null })
     return professionals
 }
 
