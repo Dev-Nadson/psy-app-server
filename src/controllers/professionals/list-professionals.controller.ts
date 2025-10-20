bc95994cd9cb4eabfade70cf9703b03dc2192ef2
Dev-Nadson <nadson.pros@gmail.com>
2025-10-20 19:13:17 -0300

    Fix: Identação corrigida

--- src/controllers/professionals/list-professionals.controller.ts
+++ src/controllers/professionals/list-professionals.controller.ts
@@ -1,6 +1,8 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
 import { list_professionals_repository } from "../../repositories/professionals/list-professionals.repo.js";
+
 async function list_professionals_controller(req: FastifyRequest, reply: FastifyReply) {
+
     const professionals = await list_professionals_repository()
     return reply.status(200).send({
         message: "Todos os usuários",
